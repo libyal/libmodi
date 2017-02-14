bee0e8bb6df4accc15bcca9c76870e49ff8ba2ce
Joachim Metz <joachim.metz@gmail.com>
2017-02-14 07:39:14 +0100

    Applied updates

--- pymodi/pymodi_handle.h
+++ pymodi/pymodi_handle.h
@@ -1,7 +1,7 @@
 /*
  * Python object definition of the libmodi handle
  *
- * Copyright (C) 2012-2016, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2012-2017, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
