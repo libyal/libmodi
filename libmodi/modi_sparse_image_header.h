0ab4d16a6749f2ba7d26275dc7bfcf9b24b24275
Joachim Metz <joachim.metz@gmail.com>
2017-12-05 06:58:54 +0100

    Applied updates

--- libmodi/modi_sparse_image_header.h
+++ libmodi/modi_sparse_image_header.h
@@ -1,7 +1,7 @@
 /*
  * The file header definition of the Mac OS sparse disk image format
  *
- * Copyright (C) 2012-2017, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2012-2018, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
