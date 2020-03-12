2f71835373d1d0bd87453ec9520f25f70509429b
Joachim Metz <joachim.metz@gmail.com>
2020-03-12 12:11:31 +0100

    Applied updates

--- tests/pymodi_test_handle.py
+++ tests/pymodi_test_handle.py
@@ -6,21 +6,22 @@
 #
 # Refer to AUTHORS for acknowledgements.
 #
-# This software is free software: you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# This software is distributed in the hope that it will be useful,
+# This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU Lesser General Public License
-# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 import os
+import random
 import sys
 import unittest
 
@@ -39,7 +40,7 @@ class HandleTypeTests(unittest.TestCase):
   def test_open(self):
     """Tests the open function."""
     if not unittest.source:
-      return
+      raise unittest.SkipTest("missing source")
 
     modi_handle = pymodi.handle()
 
@@ -59,30 +60,33 @@ class HandleTypeTests(unittest.TestCase):
   def test_open_file_object(self):
     """Tests the open_file_object function."""
     if not unittest.source:
-      return
+      raise unittest.SkipTest("missing source")
 
-    file_object = open(unittest.source, "rb")
+    if not os.path.isfile(unittest.source):
+      raise unittest.SkipTest("source not a regular file")
 
     modi_handle = pymodi.handle()
 
-    modi_handle.open_file_object(file_object)
+    with open(unittest.source, "rb") as file_object:
 
-    with self.assertRaises(IOError):
       modi_handle.open_file_object(file_object)
 
-    modi_handle.close()
+      with self.assertRaises(IOError):
+        modi_handle.open_file_object(file_object)
 
-    # TODO: change IOError into TypeError
-    with self.assertRaises(IOError):
-      modi_handle.open_file_object(None)
+      modi_handle.close()
 
-    with self.assertRaises(ValueError):
-      modi_handle.open_file_object(file_object, mode="w")
+      # TODO: change IOError into TypeError
+      with self.assertRaises(IOError):
+        modi_handle.open_file_object(None)
+
+      with self.assertRaises(ValueError):
+        modi_handle.open_file_object(file_object, mode="w")
 
   def test_close(self):
     """Tests the close function."""
     if not unittest.source:
-      return
+      raise unittest.SkipTest("missing source")
 
     modi_handle = pymodi.handle()
 
@@ -104,52 +108,90 @@ class HandleTypeTests(unittest.TestCase):
     modi_handle.open(unittest.source)
     modi_handle.close()
 
-    file_object = open(unittest.source, "rb")
+    if os.path.isfile(unittest.source):
+      with open(unittest.source, "rb") as file_object:
 
-    # Test open_file_object and close.
-    modi_handle.open_file_object(file_object)
-    modi_handle.close()
+        # Test open_file_object and close.
+        modi_handle.open_file_object(file_object)
+        modi_handle.close()
 
-    # Test open_file_object and close a second time to validate clean up on close.
-    modi_handle.open_file_object(file_object)
-    modi_handle.close()
+        # Test open_file_object and close a second time to validate clean up on close.
+        modi_handle.open_file_object(file_object)
+        modi_handle.close()
 
-    # Test open_file_object and close and dereferencing file_object.
-    modi_handle.open_file_object(file_object)
-    del file_object
-    modi_handle.close()
+        # Test open_file_object and close and dereferencing file_object.
+        modi_handle.open_file_object(file_object)
+        del file_object
+        modi_handle.close()
 
   def test_read_buffer(self):
     """Tests the read_buffer function."""
     if not unittest.source:
-      return
+      raise unittest.SkipTest("missing source")
 
     modi_handle = pymodi.handle()
 
     modi_handle.open(unittest.source)
 
-    file_size = modi_handle.get_size()
+    media_size = modi_handle.get_media_size()
 
-    # Test normal read.
-    data = modi_handle.read_buffer(size=4096)
+    if media_size < 4096:
+      # Test read without maximum size.
+      modi_handle.seek_offset(0, os.SEEK_SET)
 
-    self.assertIsNotNone(data)
-    self.assertEqual(len(data), min(file_size, 4096))
-
-    if file_size < 4096:
       data = modi_handle.read_buffer()
 
       self.assertIsNotNone(data)
-      self.assertEqual(len(data), file_size)
+      self.assertEqual(len(data), media_size)
 
-    # Test read beyond file size.
-    if file_size > 16:
-      modi_handle.seek_offset(-16, os.SEEK_END)
+    # Test read with maximum size.
+    modi_handle.seek_offset(0, os.SEEK_SET)
 
+    data = modi_handle.read_buffer(size=4096)
+
+    self.assertIsNotNone(data)
+    self.assertEqual(len(data), min(media_size, 4096))
+
+    if media_size > 8:
+      modi_handle.seek_offset(-8, os.SEEK_END)
+
+      # Read buffer on media_size boundary.
       data = modi_handle.read_buffer(size=4096)
 
       self.assertIsNotNone(data)
-      self.assertEqual(len(data), 16)
+      self.assertEqual(len(data), 8)
+
+      # Read buffer beyond media_size boundary.
+      data = modi_handle.read_buffer(size=4096)
+
+      self.assertIsNotNone(data)
+      self.assertEqual(len(data), 0)
+
+    # Stress test read buffer.
+    modi_handle.seek_offset(0, os.SEEK_SET)
+
+    remaining_media_size = media_size
+
+    for _ in range(1024):
+      read_size = int(random.random() * 4096)
+
+      data = modi_handle.read_buffer(size=read_size)
+
+      self.assertIsNotNone(data)
+
+      data_size = len(data)
+
+      if read_size > remaining_media_size:
+        read_size = remaining_media_size
+
+      self.assertEqual(data_size, read_size)
+
+      remaining_media_size -= data_size
+
+      if not remaining_media_size:
+        modi_handle.seek_offset(0, os.SEEK_SET)
+
+        remaining_media_size = media_size
 
     with self.assertRaises(ValueError):
       modi_handle.read_buffer(size=-1)
@@ -163,47 +205,80 @@ class HandleTypeTests(unittest.TestCase):
   def test_read_buffer_file_object(self):
     """Tests the read_buffer function on a file-like object."""
     if not unittest.source:
-      return
+      raise unittest.SkipTest("missing source")
 
-    file_object = open(unittest.source, "rb")
+    if not os.path.isfile(unittest.source):
+      raise unittest.SkipTest("source not a regular file")
 
     modi_handle = pymodi.handle()
 
-    modi_handle.open_file_object(file_object)
+    with open(unittest.source, "rb") as file_object:
+      modi_handle.open_file_object(file_object)
 
-    file_size = modi_handle.get_size()
+      media_size = modi_handle.get_media_size()
 
-    # Test normal read.
-    data = modi_handle.read_buffer(size=4096)
+      # Test normal read.
+      data = modi_handle.read_buffer(size=4096)
 
-    self.assertIsNotNone(data)
-    self.assertEqual(len(data), min(file_size, 4096))
+      self.assertIsNotNone(data)
+      self.assertEqual(len(data), min(media_size, 4096))
 
-    modi_handle.close()
+      modi_handle.close()
 
   def test_read_buffer_at_offset(self):
     """Tests the read_buffer_at_offset function."""
     if not unittest.source:
-      return
+      raise unittest.SkipTest("missing source")
 
     modi_handle = pymodi.handle()
 
     modi_handle.open(unittest.source)
 
-    file_size = modi_handle.get_size()
+    media_size = modi_handle.get_media_size()
 
     # Test normal read.
     data = modi_handle.read_buffer_at_offset(4096, 0)
 
     self.assertIsNotNone(data)
-    self.assertEqual(len(data), min(file_size, 4096))
+    self.assertEqual(len(data), min(media_size, 4096))
 
-    # Test read beyond file size.
-    if file_size > 16:
-      data = modi_handle.read_buffer_at_offset(4096, file_size - 16)
+    if media_size > 8:
+      # Read buffer on media_size boundary.
+      data = modi_handle.read_buffer_at_offset(4096, media_size - 8)
 
       self.assertIsNotNone(data)
-      self.assertEqual(len(data), 16)
+      self.assertEqual(len(data), 8)
+
+      # Read buffer beyond media_size boundary.
+      data = modi_handle.read_buffer_at_offset(4096, media_size + 8)
+
+      self.assertIsNotNone(data)
+      self.assertEqual(len(data), 0)
+
+    # Stress test read buffer.
+    for _ in range(1024):
+      random_number = random.random()
+
+      media_offset = int(random_number * media_size)
+      read_size = int(random_number * 4096)
+
+      data = modi_handle.read_buffer_at_offset(read_size, media_offset)
+
+      self.assertIsNotNone(data)
+
+      remaining_media_size = media_size - media_offset
+
+      data_size = len(data)
+
+      if read_size > remaining_media_size:
+        read_size = remaining_media_size
+
+      self.assertEqual(data_size, read_size)
+
+      remaining_media_size -= data_size
+
+      if not remaining_media_size:
+        modi_handle.seek_offset(0, os.SEEK_SET)
 
     with self.assertRaises(ValueError):
       modi_handle.read_buffer_at_offset(-1, 0)
@@ -220,13 +295,13 @@ class HandleTypeTests(unittest.TestCase):
   def test_seek_offset(self):
     """Tests the seek_offset function."""
     if not unittest.source:
-      return
+      raise unittest.SkipTest("missing source")
 
     modi_handle = pymodi.handle()
 
     modi_handle.open(unittest.source)
 
-    file_size = modi_handle.get_size()
+    media_size = modi_handle.get_media_size()
 
     modi_handle.seek_offset(16, os.SEEK_SET)
 
@@ -243,15 +318,16 @@ class HandleTypeTests(unittest.TestCase):
     offset = modi_handle.get_offset()
     self.assertEqual(offset, 16)
 
-    modi_handle.seek_offset(-16, os.SEEK_END)
+    if media_size > 16:
+      modi_handle.seek_offset(-16, os.SEEK_END)
 
-    offset = modi_handle.get_offset()
-    self.assertEqual(offset, file_size - 16)
+      offset = modi_handle.get_offset()
+      self.assertEqual(offset, media_size - 16)
 
     modi_handle.seek_offset(16, os.SEEK_END)
 
     offset = modi_handle.get_offset()
-    self.assertEqual(offset, file_size + 16)
+    self.assertEqual(offset, media_size + 16)
 
     # TODO: change IOError into ValueError
     with self.assertRaises(IOError):
@@ -259,11 +335,11 @@ class HandleTypeTests(unittest.TestCase):
 
     # TODO: change IOError into ValueError
     with self.assertRaises(IOError):
-      modi_handle.seek_offset(-32 - file_size, os.SEEK_CUR)
+      modi_handle.seek_offset(-32 - media_size, os.SEEK_CUR)
 
     # TODO: change IOError into ValueError
     with self.assertRaises(IOError):
-      modi_handle.seek_offset(-32 - file_size, os.SEEK_END)
+      modi_handle.seek_offset(-32 - media_size, os.SEEK_END)
 
     # TODO: change IOError into ValueError
     with self.assertRaises(IOError):
@@ -274,6 +350,52 @@ class HandleTypeTests(unittest.TestCase):
     # Test the seek without open.
     with self.assertRaises(IOError):
       modi_handle.seek_offset(16, os.SEEK_SET)
+
+  def test_get_offset(self):
+    """Tests the get_offset function."""
+    if not unittest.source:
+      raise unittest.SkipTest("missing source")
+
+    modi_handle = pymodi.handle()
+
+    modi_handle.open(unittest.source)
+
+    offset = modi_handle.get_offset()
+    self.assertIsNotNone(offset)
+
+    modi_handle.close()
+
+  def test_get_media_size(self):
+    """Tests the get_media_size function and media_size property."""
+    if not unittest.source:
+      raise unittest.SkipTest("missing source")
+
+    modi_handle = pymodi.handle()
+
+    modi_handle.open(unittest.source)
+
+    media_size = modi_handle.get_media_size()
+    self.assertIsNotNone(media_size)
+
+    self.assertIsNotNone(modi_handle.media_size)
+
+    modi_handle.close()
+
+  def test_get_image_type(self):
+    """Tests the get_image_type function and image_type property."""
+    if not unittest.source:
+      raise unittest.SkipTest("missing source")
+
+    modi_handle = pymodi.handle()
+
+    modi_handle.open(unittest.source)
+
+    image_type = modi_handle.get_image_type()
+    self.assertIsNotNone(image_type)
+
+    self.assertIsNotNone(modi_handle.image_type)
+
+    modi_handle.close()
 
 
 if __name__ == "__main__":
