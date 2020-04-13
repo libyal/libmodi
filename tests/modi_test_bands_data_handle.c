758c8c42f72379582c7974917d4bd115bc291223
Joachim Metz <joachim.metz@gmail.com>
2020-04-13 16:57:17 +0200

    Applied updates

---
+++ tests/modi_test_bands_data_handle.c
@@ -0,0 +1,116 @@
+/*
+ * Library bands_data_handle type test program
+ *
+ * Copyright (C) 2012-2020, Joachim Metz <joachim.metz@gmail.com>
+ *
+ * Refer to AUTHORS for acknowledgements.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <common.h>
+#include <file_stream.h>
+#include <types.h>
+
+#if defined( HAVE_STDLIB_H ) || defined( WINAPI )
+#include <stdlib.h>
+#endif
+
+#include "modi_test_libcerror.h"
+#include "modi_test_libmodi.h"
+#include "modi_test_macros.h"
+#include "modi_test_memory.h"
+#include "modi_test_unused.h"
+
+#include "../libmodi/libmodi_bands_data_handle.h"
+
+#if defined( __GNUC__ ) && !defined( LIBMODI_DLL_IMPORT )
+
+/* Tests the libmodi_bands_data_handle_free function
+ * Returns 1 if successful or 0 if not
+ */
+int modi_test_bands_data_handle_free(
+     void )
+{
+	libcerror_error_t *error = NULL;
+	int result               = 0;
+
+	/* Test error cases
+	 */
+	result = libmodi_bands_data_handle_free(
+	          NULL,
+	          &error );
+
+	MODI_TEST_ASSERT_EQUAL_INT(
+	 "result",
+	 result,
+	 -1 );
+
+	MODI_TEST_ASSERT_IS_NOT_NULL(
+	 "error",
+	 error );
+
+	libcerror_error_free(
+	 &error );
+
+	return( 1 );
+
+on_error:
+	if( error != NULL )
+	{
+		libcerror_error_free(
+		 &error );
+	}
+	return( 0 );
+}
+
+#endif /* defined( __GNUC__ ) && !defined( LIBMODI_DLL_IMPORT ) */
+
+/* The main program
+ */
+#if defined( HAVE_WIDE_SYSTEM_CHARACTER )
+int wmain(
+     int argc MODI_TEST_ATTRIBUTE_UNUSED,
+     wchar_t * const argv[] MODI_TEST_ATTRIBUTE_UNUSED )
+#else
+int main(
+     int argc MODI_TEST_ATTRIBUTE_UNUSED,
+     char * const argv[] MODI_TEST_ATTRIBUTE_UNUSED )
+#endif
+{
+	MODI_TEST_UNREFERENCED_PARAMETER( argc )
+	MODI_TEST_UNREFERENCED_PARAMETER( argv )
+
+#if defined( __GNUC__ ) && !defined( LIBMODI_DLL_IMPORT )
+
+	/* TODO: add tests for libmodi_bands_data_handle_initialize */
+
+	MODI_TEST_RUN(
+	 "libmodi_bands_data_handle_free",
+	 modi_test_bands_data_handle_free );
+
+	/* TODO: add tests for libmodi_bands_data_handle_append_segment */
+
+	/* TODO: add tests for libmodi_bands_data_handle_read_segment_data */
+
+	/* TODO: add tests for libmodi_bands_data_handle_seek_segment_offset */
+
+#endif /* defined( __GNUC__ ) && !defined( LIBMODI_DLL_IMPORT ) */
+
+	return( EXIT_SUCCESS );
+
+on_error:
+	return( EXIT_FAILURE );
+}
+
