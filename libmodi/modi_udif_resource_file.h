8ffbf5d20c198b368d990f3db5827aa586ef0510
Joachim Metz <joachim.metz@gmail.com>
2021-01-11 18:49:56 +0100

    Applied updates

--- libmodi/modi_udif_resource_file.h
+++ libmodi/modi_udif_resource_file.h
@@ -1,7 +1,7 @@
 /*
  * The resource file definition of the Mac OS Universal Disk Image Format (UDIF)
  *
- * Copyright (C) 2012-2020, Joachim Metz <joachim.metz@gmail.com>
+ * Copyright (C) 2012-2021, Joachim Metz <joachim.metz@gmail.com>
  *
  * Refer to AUTHORS for acknowledgements.
  *
