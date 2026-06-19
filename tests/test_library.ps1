6cdcaf999bc6e064031090bc5c5dc0332814a21b
Joachim Metz <joachim.metz@gmail.com>
2026-06-19 06:39:35 +0200

    Worked on tests and changes for CI tests

--- tests/test_library.ps1
+++ tests/test_library.ps1
@@ -1,71 +1,10 @@
 # Tests library functions and types.
-#
-# Version: 20260615
 
 $LibraryTests = "bands_data_handle bit_stream bzip data_block deflate error huffman_tree io_handle notify sparse_bundle_xml_plist sparse_image_header system_string udif_block_table udif_block_table_entry udif_resource_file udif_xml_plist"
 $LibraryTestsWithInput = "handle support"
 $OptionSets = "" -split " "
 
 . .\test_functions.ps1
-
-Function RunTest
-{
-	param( [string]$TestName )
-
-	$TestBinary = "modi_test_${TestName}"
-
-	$TestDescription = "${TestBinary}"
-	$TestExecutable = "${TestExecutablesDirectory}\${TestBinary}.exe"
-
-	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
-	{
-		WriteTestResult ${TestDescription} ${ExitIgnore}
-
-		Return ${ExitIgnore}
-	}
-	$Output = Invoke-Expression ${TestExecutable}
-	$Result = $global:LastExitCode
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host ${Output} -foreground Red
-	}
-	WriteTestResult ${TestDescription} ${Result}
-
-	Return ${Result}
-}
-
-Function RunTestWithInput
-{
-	param( [string]$TestName, [string[]]$TestInput )
-
-	$OptionSet = $TestInput[0]
-	$Options = $TestInput[1]
-	$TestFile = $TestInput[2]
-
-	$TestBinary = "modi_test_${TestName}"
-	$TestFileName = (${TestFile} -split '\\')[-2..-1] -join '\'
-
-	$TestDescription = "${TestBinary} with input: '${TestFileName}"
-	$TestExecutable = "${TestExecutablesDirectory}\${TestBinary}.exe"
-
-	If (-Not (Test-Path -Path ${TestExecutable} -PathType Leaf))
-	{
-		WriteTestResult ${TestDescription} ${ExitIgnore}
-
-		Return ${ExitIgnore}
-	}
-	$Output = Invoke-Expression "${TestExecutable} ${Options} ${TestFile}"
-	$Result = $global:LastExitCode
-
-	If (${Result} -ne ${ExitSuccess})
-	{
-		Write-Host ${Output} -foreground Red
-	}
-	WriteTestResult ${TestDescription} ${Result}
-
-	Return ${Result}
-}
 
 $TestExecutablesDirectory = GetTestExecutablesDirectory
 
@@ -85,7 +24,7 @@ Foreach (${TestName} in ${LibraryTests} -split " ")
 	{
 		Continue
 	}
-	$Result = RunTest ${TestName}
+	$Result = RunTestBinary ${TestExecutablesDirectory} "modi_test_${TestName}"
 
 	If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
 	{
@@ -104,7 +43,7 @@ Foreach (${TestName} in ${LibraryTestsWithInput} -split " ")
 	}
 	ForEach ($TestInput in ${TestInputs})
 	{
-		$Result = RunTestWithInput ${TestName} ${TestInput}
+		$Result = RunTestBinaryWithInput ${TestExecutablesDirectory} "modi_test_${TestName}" ${TestInput}
 
 		If ((${Result} -ne ${ExitSuccess}) -And (${Result} -ne ${ExitIgnore}))
 		{
